6006cb59698876d0583cbc0106d013a6c3c9518c
MrSoundWAVEog <jadly129131@gmail.com>
2026-02-11 20:02:06 +0600

    Kbuild: link-vmlinux: fix up LTO for phoenix-bpf-5.10 when building with LTO, it always failed with:
    ../scripts/generate_initcall_order.pl: object file ./built-in.a doesn't exist? at ../scripts/generate_initcall_order.pl line 42.
    llvm-ar: error: unable to load 'built-in.a': No such file or directory
      MODPOST vmlinux.o
    
    so it wasn't able to generate the initcall.
    
    looking on aosp kernel_common however its like
    
    aosp-mirror/kernel_common@a8cccdd
    
    so it is called like
    ```
    generate_initcall_order.pl ${KBUILD_VMLINUX_OBJS} ${KBUILD_VMLINUX_LIBS} > .tmp_initcalls.lds
    ```
    
    however we don't have ${KBUILD_VMLINUX_OBJS} so we see what happened to it
    
    gregkh/linux@d151e97
    
    $(KBUILD_VMLINUX_OBJS) is just $(KBUILD_VMLINUX_INIT) $(KBUILD_VMLINUX_MAIN)

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -80,11 +80,11 @@ lto_lds()
 	fi
 
 	${srctree}/scripts/generate_initcall_order.pl \
-		built-in.a ${KBUILD_VMLINUX_LIBS} \
+		${KBUILD_VMLINUX_INIT} ${KBUILD_VMLINUX_MAIN} ${KBUILD_VMLINUX_LIBS} \
 		> .tmp_lto.lds
 
 	if [ -n "${CONFIG_MODVERSIONS}" ]; then
-		for a in built-in.a ${KBUILD_VMLINUX_LIBS}; do
+		for a in ${KBUILD_VMLINUX_INIT} ${KBUILD_VMLINUX_MAIN} ${KBUILD_VMLINUX_LIBS}; do
 			for o in $(${AR} t $a); do
 				if [ -f ${o}.symversions ]; then
 					cat ${o}.symversions >> .tmp_lto.lds
